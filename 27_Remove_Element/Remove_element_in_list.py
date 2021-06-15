7a4b1f29c359f083242a1ce80a58a20fd2f4fa8a
binruli <lbycloud@gmail.com>
2021-06-15 18:33:57 -0500

    LeetCode solution 27
    Remove Element

---
+++ 27_Remove_Element/Remove_element_in_list.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for numbers in range(len(nums)):
+            if val not in nums:
+                break
+            if len(nums) == 0:
+                return 0
+            else:
+                nums.remove(val)
+            
+        print(len(nums))
+        print ("nums = ",nums)
